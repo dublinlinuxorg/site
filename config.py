d9cddcd7f80e05d6c949e3b483f60c81163bea2e
mike <mike@mykolab.ch>
2021-07-24 00:58:10 +0100

    changing live dir back to public as gitlab pages requires that

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ config = {
 	'md_folder': 'md_files',
 	'theme_folder': Path('themes', f'{site_config["theme"]}'),
 	'template_folder': Path('themes', f'{site_config["theme"]}', 'templates'),
-	'live_folder': 'live',
+	'live_folder': 'public', # this has to be called public because of GitLab pages
 	'output_folder': 'output',
     'dev_url': '127.0.0.1',
     'dev_port': 5555
